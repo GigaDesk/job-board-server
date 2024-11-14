928999b9290000ecec8283769eb66154f6e3df63
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-11-14 16:50:37 +0300

    feat: define resolver functions for school schema

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/GigaDesk/eardrum-server/graph/db"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct{
+	Sql *db.AutoGqlDB // this is the new line the package db is autogenerate by this plugin
+}
