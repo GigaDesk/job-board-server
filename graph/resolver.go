6b3fe60275c50ff1560d5d31670f1632f48f1055
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-11-28 14:48:13 +0300

    feat: initialize neo4j database connection

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,6 +1,9 @@
 package graph
 
-import "github.com/GigaDesk/eardrum-server/graph/db"
+import (
+	"github.com/GigaDesk/eardrum-graph/neo4jutils"
+	"github.com/GigaDesk/eardrum-server/graph/db"
+)
 
 // This file will not be regenerated automatically.
 //
@@ -8,4 +11,5 @@ import "github.com/GigaDesk/eardrum-server/graph/db"
 
 type Resolver struct{
 	Sql *db.AutoGqlDB // this is the new line the package db is autogenerate by this plugin
+	Neo4j *neo4jutils.Neo4jInstance
 }
--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/GigaDesk/eardrum-server/auth"
+	"github.com/GigaDesk/eardrum-graph/neo4jutils"
 	"github.com/GigaDesk/eardrum-server/database/postgreutils"
 	"github.com/GigaDesk/eardrum-server/graph"
 	"github.com/GigaDesk/eardrum-server/phoneutils"
@@ -16,7 +17,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var postgresInstance postgreutils.PostgresInstance
+var (
+	postgresInstance postgreutils.PostgresInstance
+	neo4jInstance  neo4jutils.Neo4jInstance
+)
+
 
 func main() {
 	// Find .env file
@@ -31,13 +36,20 @@ func main() {
 	defaultPort := os.Getenv("DEFAULT_PORT")
 
 	postgresInstance.Init(os.Getenv("POSTGRES_DBURL"))
+	neo4jerr:=neo4jInstance.Init(os.Getenv("NEO4J_DBURI"), os.Getenv("NEO4J_DBUSER"), os.Getenv("NEO4J_DBPASSWORD"))
+
+	if neo4jerr !=nil {
+		log.Println("problem initializing neo4j database: ", neo4jerr)
+	}
+	
+	defer neo4jInstance.Driver.Close(neo4jInstance.Ctx)
 
 	port := defaultPort
 	router := chi.NewRouter()
 	router.Use(auth.Middleware())
 
 	//srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{Sql: &dborm}})) //.... <- here set dborm to resolver
-	server := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{Sql: &postgresInstance.Dborm}}))
+	server := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{Sql: &postgresInstance.Dborm, Neo4j: &neo4jInstance }}))
 
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", server)
